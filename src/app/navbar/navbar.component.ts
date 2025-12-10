f1d88b74b132d023dfe95cf1b39c1b373bbc3da2
tonkla785 <tonkla785@gmail.com>
2025-12-10 15:10:38 +0700

    install_bootstrap
    setup project bootstrap & link component

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { InputDetailComponent } from '../input-detail/input-detail.component';
+import { ConfirmReviewComponent } from '../confirm-review/confirm-review.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [InputDetailComponent ,ConfirmReviewComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
